0e82520d0e5e5a1e749000a8e34f8505623e9d13
yzh119 <expye@outlook.com>
2019-08-21 20:45:44 +0800

    upd scripts

--- run_benchmarks.sh
+++ run_benchmarks.sh
@@ -1,13 +1,12 @@
+echo "dataset, hidden-size, hidden-layers, tvm speed(ms), dgl speed(ms)"
 for data in cora citeseer pubmed; do
   for D in 16 32 64 128; do
     for L in 1 5 10 15 20; do
-      echo $data, $D, $L
       python tvm_gcn.py -data $data -d $D -l $L >/dev/null 2>/dev/null
-      outdir="logs/tvm_num_hidden-"$L"_hidden_dim-"$D"_dataset-"$data
-      echo "tvm speed: " $(cat $outdir) ms 
-      outdir="logs/dgl_num_hidden-"$L"_hidden_dim-"$D"_dataset-"$data
       python dgl_gcn.py -data $data -d $D -l $L >/dev/null 2>/dev/null
-      echo "dgl speed: " $(cat $outdir) ms
+      tvmdir="logs/tvm_num_hidden-"$L"_hidden_dim-"$D"_dataset-"$data
+      dgldir="logs/dgl_num_hidden-"$L"_hidden_dim-"$D"_dataset-"$data
+      echo $data, $D, $L, $(cat $tvmdir), $(cat $dgldir)
     done
   done
 done
